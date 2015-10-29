ff4f929c7f69d4d2ef391023210d89be4e8b3e95
Michael Reed <myk27x@yahoo.com>
2015-10-29 18:14:05 -0400

    created table associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :receiver
+  has_one :donor
+  has_one :transporter
 end
