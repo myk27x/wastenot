64b02dde1dc1e2b37b29c2461e66bc38d1b9bbb5
Michael Reed <myk27x@yahoo.com>
2015-10-29 18:09:33 -0400

    cell phone cannot be integer, changed to string

--- db/migrate/20151028033459_create_transporters.rb
+++ db/migrate/20151028033459_create_transporters.rb
@@ -1,11 +1,11 @@
 class CreateTransporters < ActiveRecord::Migration
   def change
     create_table :transporters do |t|
-      t.integer :user_id
-      t.string :name
-      t.integer :cell_phone
-      t.time :open
-      t.time :close
+      t.integer  :user_id
+      t.string   :name
+      t.string   :cell_phone
+      t.time     :open
+      t.time     :close
 
       t.timestamps null: false
     end
