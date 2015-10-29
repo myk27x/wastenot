d51f92d2b7d72f215c9b3d0058dc549ca2ceebaa
Michael Reed <myk27x@yahoo.com>
2015-10-29 18:15:53 -0400

    added table associations and removed cell carrier

--- app/models/transporter.rb
+++ app/models/transporter.rb
@@ -1,4 +1,5 @@
 class Transporter < ActiveRecord::Base
-  validates :cell_phone,   presence: true, uniqueness: true, length: { is: 10 }
-  validates :cell_carrier, presence: true
+  validates :cell_phone, presence: true, uniqueness: true
+
+  belongs_to :user
 end
