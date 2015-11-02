48a07f1675446975ccbc60be7aa40494e4ae5e98
Michael Reed <myk27x@yahoo.com>
2015-11-02 11:52:32 -0500

    modified sender with message

--- app/models/pickup_notice_sender.rb
+++ app/models/pickup_notice_sender.rb
@@ -1,6 +1,10 @@
-class PickupNoticeSender
-  def notify(contact_data, pickup_data)
+class PickupNotice
+  def send_notice(contact_data)
     client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     message = client.account.messages.create from: ENV['TWILIO_SMS_NUMBER'], to: contact_data, body: pickup_data
+  end
+
+  def pickup_data
+    "#WASTENOT has a pick-up! Are you available? #weblink here?"
   end
 end
