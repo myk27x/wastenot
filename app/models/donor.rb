bab3327ede198e000d1961f7e02b2d10d265f316
Michael Reed <myk27x@yahoo.com>
2015-11-03 12:04:22 -0500

    added model associations

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,2 +1,7 @@
 class Donor < ActiveRecord::Base
+  has_many :donations
+
+  def self.anonymous
+    Donor.find_by(name: "Anonymous")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   has_one :receiver
   has_one :donor
   has_one :transporter
-
+  has_many :donations, through: :donor
 end
