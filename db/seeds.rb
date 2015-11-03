c318b63656e4fe5ceda48fc55f40a14205db3e06
Michael Reed <myk27x@yahoo.com>
2015-11-03 12:01:28 -0500

    added seeds for demo purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: 'myk27x@yahoo.com', password: 'bananas27')
-Transporter.create(user_id: 1, name: 'Michael Reed', cell_phone: '7274235459')
+User.create(email: 'noone@example.com', password: 'sekret27')
+Donor.create(user_id: 1, org_name: 'thin air', street_address: '123 Main St.', city: 'Who-ville', state: 'Speck', zip: '14303-327', contact_name: 'Mayor', phone: '+12-34-56-78-90')
+
+User.create(email: 'michael@example.com', password: 'reed1234')
+Transporter.create(user_id: 2, name: 'Michael Reed', cell_phone: '+17274235459', open: 19, close: 23)
+
+User.create(email: 'kristen@example.com', password: 'swan1234')
+Receiver.create(user_id: 3, org_name: 'Swan Services', street_address: '456 7th St.', city: 'Townsville', state: 'Tokyo', zip: '100-0001-1', contact_name: 'Yuuko Takanashi', phone: '012-3456-7890', open: 6, close: 22, delivery_instructions: 'Make inside package. Send for Japan. Thanking to your.')
