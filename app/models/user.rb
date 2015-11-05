0ddc1fdb0ab6d012df103cccd86805d3485eac7b
Michael Reed <myk27x@yahoo.com>
2015-11-05 13:34:35 -0500

    fixed indents

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :receiver
-  has_one :donor
-  has_one :transporter
+  has_one  :receiver
+  has_one  :donor
+  has_one  :transporter
   has_many :donations, through: :donor
 end
