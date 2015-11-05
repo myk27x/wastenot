783ff2ff0cfd8981a9db66c7df388deb9a107cbe
Michael Reed <myk27x@yahoo.com>
2015-11-05 13:11:35 -0500

    seeding for shelters

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,17 @@ User.create(email: 'noone@example.com', password: 'sekret27')
 Donor.create(user_id: 1, org_name: 'thin air', street_address: '123 Main St.', city: 'Who-ville', state: 'Speck', zip: '14303-327', contact_name: 'Mayor', phone: '+12-34-56-78-90')
 
 User.create(email: 'michael@example.com', password: 'reed1234')
-Transporter.create(user_id: 2, name: 'Michael Reed', cell_phone: '+17274235459', open: 19, close: 23)
+Donor.create(user_id: 2, org_name: 'Michael and Carly')
+Transporter.create(user_id: 2, name: 'Michael Reed', cell_phone: '+17274235459', open: 0, close: 24)
 
 User.create(email: 'kristen@example.com', password: 'swan1234')
-Receiver.create(user_id: 3, org_name: 'Swan Services', street_address: '456 7th St.', city: 'Townsville', state: 'Tokyo', zip: '100-0001-1', contact_name: 'Yuuko Takanashi', phone: '012-3456-7890', open: 6, close: 22, delivery_instructions: 'Make inside package. Send for Japan. Thanking to your.')
+# Receiver.create(user_id: 3, org_name: 'Swan Services', street_address: '456 7th St.', city: 'Townsville', state: 'Tokyo', zip: '100-0001-1', contact_name: 'Yuuko Takanashi', phone: '012-3456-7890', open: 6, close: 22, delivery_instructions: 'Make inside package. Send for Japan. Thanking to your.')
+
+User.create(email: 'stvin@example.com', password: 'depaul12')
+Receiver.create(user_id: 4, org_name: 'St Vincent De Paul Society', street_address: '384 15th St N, St Petersburg, FL 33705', contact_name: 'Bishop Pastor', phone: '(727)555-1212', open: 6, close: 22, delivery_instructions: 'Entrance faces North underneath the overpass from 15th St. Pull in, park, come up the ramp and introduce yourself first, please.')
+
+User.create(email: 'womans@example.com', password: 'residence')
+Receiver.create(user_id: 5, org_name: 'Womans Residence', street_address: '814 4th Ave N, St Petersburg, FL 33701', contact_name: 'Caring Lady', phone: '(727)555-1212', open:17, close:22, delivery_instructions: 'Bring all donations around back. Knock 3 times, 2 times, 3 times, and say the password: Mellon. Someone will open the door slowly and greet you with: Welcome #wastenot...')
+
+User.create(email: 'alpha@example.com', password: 'house123')
+Receiver.create(user_id: 6, org_name: 'Alpha House of Pinellas County, Inc.', street_address: '701 5th Ave N, St Petersburg, FL 33701', contact_name: 'Alpha Owner', phone: '(727)555-1212', open:6, close:22, delivery_instructions: 'Pull up to the front of the building and in Marco-Polo fashion yell out: BETA! When you hear the response: ALPHA!, someone will be down to see you in a moment.')
