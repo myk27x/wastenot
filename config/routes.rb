64048f9930efd5ee2a281b680261f6020dbc5544
Michael Reed <myk27x@yahoo.com>
2015-11-04 19:03:53 -0500

    changing root from home to slash

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
 
-  root to: "home#index"
+  root to: "/"
 
   resources :receivers
 
