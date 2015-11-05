56b3d36a93f493aa3e5f370f695bcddd2fb352f9
Michael Reed <myk27x@yahoo.com>
2015-11-04 19:05:15 -0500

    ok, how about this

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
 
-  root to: "/"
+  # root to: "/"
 
   resources :receivers
 
