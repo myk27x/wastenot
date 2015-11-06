85bc95c0c0f8731d0b214798be059e4af98e195b
Michael Reed <myk27x@yahoo.com>
2015-11-05 20:06:33 -0500

    removing devise from users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
   has_one  :receiver
   has_one  :donor
   has_one  :transporter
