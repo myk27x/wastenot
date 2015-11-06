864e8a2fe8c74692c6352192ffe98d29f5312f4b
Michael Reed <myk27x@yahoo.com>
2015-11-06 16:25:35 -0500

    adds instruction validation for donations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,7 +1,8 @@
 class Donation < ActiveRecord::Base
   belongs_to :donor
 
-  validates :address, presence: true
+  validates :address,      presence: true
+  validates :instructions, presence: true
 
   geocoded_by      :address
   after_validation :geocode
