49689b0252f064a00a93e5d3d2129cc8d34b31bd
Michael Reed <myk27x@yahoo.com>
2015-11-06 21:51:29 -0500

    responds as json with session for testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      # render json: session, status: 202
-      render nothing: true
+      render json: session, status: 202
+      # render nothing: true
     else
       render nothing: true, status: 400
     end
