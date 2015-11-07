3c6044096bafd1a6f48c4939cc329bf060be2181
Michael Reed <myk27x@yahoo.com>
2015-11-07 12:38:32 -0500

    sends user params back for render of show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,17 @@ class UsersController < ApplicationController
   before_filter :authorize, only: :show
 
   def show
-    @user = User.find(current_user.id)
-    render json: @user
+    user = User.find(current_user.id)
+    render json: user
   end
 
   def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      response status: 201
+      render json: user, status: 201
     else
-      redirect_to root_path, status: 400
+      render status: 400
     end
   end
 
