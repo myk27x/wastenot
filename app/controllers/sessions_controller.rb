38fd7566d51046053d3795f11cbbe1c0b93149ec
Michael Reed <myk27x@yahoo.com>
2015-11-07 12:43:15 -0500

    returning user params for show page on signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: session, status: 202
+      render json: user, status: 202
       # render nothing: true
     else
       render nothing: true, status: 400
