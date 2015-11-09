9b97bdd39ca2f5aba6e0037b5a0bbb95f26cea40
Michael Reed <myk27x@yahoo.com>
2015-11-09 14:40:46 -0500

    removed helper method for current user due to passing params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def current_user(user_id)
     @current_user ||= User.find(user_id)
   end
-  helper_method :current_user
+  # helper_method :current_user
 
   def authorize
     redirect_to root_path, status: 401 unless current_user
