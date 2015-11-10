354286be5e7286b18bc43e6a733c666cdb501050
Michael Reed <myk27x@yahoo.com>
2015-11-09 22:01:37 -0500

    needs to have user auth added
    removed user auth for time being

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   respond_to :json, :html
-  # before_filter :authorize
 
   def current_user(user_id)
     @current_user ||= User.find(user_id)
   end
-  # helper_method :current_user
 
-  def authorize
-    redirect_to root_path, status: 401 #unless current_user
-  end
+  # TODO This needs to be remedied ASAP before going live
+  # def authorize
+  #   render status: 401 #unless current_user
+  # end
 end
